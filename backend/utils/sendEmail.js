a47cd2fec793e95b157793544f22746d21b26541
Sameer-11-16 <sameeransari00000@gmail.com>
2026-04-14 00:52:48 +0530

    Feature: Switch to Gmail OAuth2 API to bypass Render SMTP block

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,26 +1,39 @@
 const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
 
-const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false, // Use STARTTLS
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    },
-    tls: {
-        rejectUnauthorized: false
-    },
-    // Master Override: Force DNS to only use IPv4
-    lookup: (hostname, options, callback) => {
-        require('dns').lookup(hostname, { family: 4 }, callback);
-    }
-});
+const OAuth2 = google.auth.OAuth2;
+
+const createTransporter = async () => {
+    const oauth2Client = new OAuth2(
+        process.env.GMAIL_CLIENT_ID,
+        process.env.GMAIL_CLIENT_SECRET,
+        'https://developers.google.com/oauthplayground'
+    );
+
+    oauth2Client.setCredentials({
+        refresh_token: process.env.GMAIL_REFRESH_TOKEN
+    });
+
+    const accessToken = await oauth2Client.getAccessToken();
+
+    return nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            type: 'OAuth2',
+            user: process.env.EMAIL_USER,
+            clientId: process.env.GMAIL_CLIENT_ID,
+            clientSecret: process.env.GMAIL_CLIENT_SECRET,
+            refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+            accessToken: accessToken.token
+        }
+    });
+};
 
 const sendEmail = async (options) => {
+    const transporter = await createTransporter();
 
     const mailOptions = {
-        from: `"Antigravity Food" <${process.env.EMAIL_USER}>`,
+        from: `"BiteStream Food" <${process.env.EMAIL_USER}>`,
         to: options.email,
         subject: options.subject,
         html: options.html
